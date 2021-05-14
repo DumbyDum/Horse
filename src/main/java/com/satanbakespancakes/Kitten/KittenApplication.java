1f88590b9bdab33b7f4f15f19e1748ae6ea43740
DumbyDum <akademike2005@mail.ru>
2021-05-15 01:40:26 +0300

    first commit

--- src/main/java/com/satanbakespancakes/Kitten/KittenApplication.java
+++ src/main/java/com/satanbakespancakes/Kitten/KittenApplication.java
@@ -1,19 +1,52 @@
 package com.satanbakespancakes.Kitten;
 
+
+import org.apache.tomcat.jni.Buffer;
+import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import java.awt.*;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Random;
 
 @Controller
 @SpringBootApplication
 public class KittenApplication {
 
+	@RequestMapping(
+			value = "/kitten",
+			method= RequestMethod.GET,
+			produces = MediaType.IMAGE_JPEG_VALUE)
+	@ResponseBody
+	ResponseEntity<byte[]> getKitten() throws IOException {
+		Resource img = new ClassPathResource("src/main/resources/static/kitten.jpg");
+		byte[] bytes = StreamUtils.copyToByteArray(img.getInputStream());
+
+		return ResponseEntity.ok()
+				.contentType(MediaType.IMAGE_JPEG)
+				.body(bytes);
+	}
+
+	@RequestMapping("/gaussian")
+	@ResponseBody
+	String getGaussian(){
+		return Double.toString(new Random().nextGaussian());
+	}
+
 	@RequestMapping("/")
 	@ResponseBody
-	String home(){
-		return "hello";
+	String hello(){
+		return "Hello!";
 	}
 
 	public static void main(String[] args) {
